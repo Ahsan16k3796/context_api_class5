193ee068badcba32d571c129d643096beea1353e
Ahsan Ali <ahxank47@gmail.com>
2020-07-17 18:40:42 +0500

    Modified

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Parent from './parent'
+import ValueContext from './ValueContext';
 
 function App() {
   let [number,setnumber]=useState(45);
   
   return (
-  <div> 
-    Number in App.js is: {number}
-    <Parent num={number}></Parent>
-    <button onClick={()=>(setnumber(++number))}>
-      +
-    </button>
-  </div>
+<ValueContext.Provider value={78}>    
+   <div> 
+     Number in App.js is: {number}
+     <Parent></Parent>
+     {/* <button onClick={()=>(setnumber(++number))}>
+       +
+     </button> */}
+   </div>
+  </ValueContext.Provider>
 
   );
 }
--- src/child.js
+++ src/child.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import Parent from './parent'
+import ValueContext from './ValueContext';
 
 function Child(props) {
-  
+  let val = useContext(ValueContext);
   return (
   <div> 
-    The number inherited from Parent and upper levels is: {props.num}
+    The number inherited from Parent and upper levels is: {val}
   </div>
   );
 }
